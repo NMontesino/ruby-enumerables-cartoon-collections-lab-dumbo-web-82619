724999017c06e9391ca386733b95f68c4da9518f
glorious-genre-8033 <namontesino@gmail.com>
2019-08-17 00:18:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,10 +22,17 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
   i = 0
   
   while i < array.length do
-    if cheese_types.include
+    if cheese_types.include?(array[i])
+      return array[i]
+      i += 1
+    end
   end
+  
+  return nil
+  
 end
