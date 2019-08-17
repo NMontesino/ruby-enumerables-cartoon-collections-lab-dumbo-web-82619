4d2e76449f9af44c6dfa04a44557775e81fc7cc0
glorious-genre-8033 <namontesino@gmail.com>
2019-08-17 00:08:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,13 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  # Your code here
+  i = 0 
+  while i < array.length do
+    if array[i].length > 4
+      return true
+    end
+  end
+  return false
 end
 
 def find_the_cheese# code an argument here
